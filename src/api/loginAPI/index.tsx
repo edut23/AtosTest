bfbc63b3996cbedd3dfe3a29ceaf1118d87ab73a
Edu Teodoro <edu.teodoro@tebesensor.com>
2024-02-29 11:33:53 -0300

    removeWarning

--- src/api/loginAPI/index.tsx
+++ src/api/loginAPI/index.tsx
@@ -1,7 +1,4 @@
-import { useState } from "react";
 import { Api } from "../axios-config";
-//import axios from "axios";
-//import MockAdapter from "axios-mock-adapter";
 
 interface Products{
     id: number,
@@ -55,20 +52,3 @@ export const loginApi = async (username: string, password: string): Promise<Item
         return new Error((error as { message: string}).message || 'Listing movies error.')
     }
 };
-
-/*describe('Login', () => {
-    it('should successfully log in and return token', async () => {
-      // Criar uma instância do mock adapter
-      const mock = new MockAdapter(axios);
-  
-      // Definir a resposta do mock para a rota de login
-      const mockToken = 'mocked_token';
-      mock.onPost('/user').reply(200, { token: mockToken });
-  
-      // Chamar a função de login
-      const token = await loginApi('username', 'password');
-  
-      // Verificar se a função de login retornou o token esperado
-      expect(token).toEqual(mockToken);
-    });
-  });*/
